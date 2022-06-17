e5ddc34a5d13b1430ac9e49b61bf45b1042feb2e
Diogo Loiola <diogomouraloiola@gmail.com>
2022-06-17 12:00:15 -0300

    add callbacks to save coordinates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,14 @@
 class Address < ApplicationRecord
+  attr_accessor :coordinates
+
   belongs_to :partner
+
+  before_save :parse_coordinates
+
+  private
+
+  def parse_coordinates
+    self.latitude = coordinates[0]
+    self.longitude = coordinates[1]
+  end
 end
