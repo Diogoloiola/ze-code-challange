489f7393dae7ef4186e59529645e448004b49cdb
Diogo Loiola <diogomouraloiola@gmail.com>
2022-06-17 12:54:50 -0300

    add filter by coordinates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,11 @@
 class Address < ApplicationRecord
   attr_accessor :coordinates
 
+  reverse_geocoded_by :latitude, :longitude
+
   belongs_to :partner
+
+  scope :filter_by_coords, ->(point) { near(point.split(','), 9_999_999, order: :distance) }
 
   before_save :parse_coordinates
 
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,11 +1,17 @@
 class Partner < ApplicationRecord
-  before_save :json_to_string
+  include Filterable
+
+  has_many :addresses
+  accepts_nested_attributes_for :addresses
 
   validates :trading_name, :owner_name, :coverage_area, presence: true
   validates :document, uniqueness: true
 
-  has_many :addresses
-  accepts_nested_attributes_for :addresses
+  before_save :json_to_string
+
+  scope :filter_by_coordinates, lambda { |value|
+    where(id: Address.filter_by_coords(value).pluck(:id))
+  }
 
   def parse_coordinates
     JSON.parse(coverage_area.gsub(/:([a-zA-z]+)/, '"\\1"').gsub('=>', ': ')).symbolize_keys
